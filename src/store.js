a652f85cb72a1c98385e51846a64cba2f43bfa77
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-22 14:35:21 +0000

    finished connecting state in Vuex to events including eventShow component pagination in eventList component limiting api calls for eventShow component by setting data in store once it's retreived by the api

--- src/store.js
+++ src/store.js
@@ -8,11 +8,22 @@ export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Adam Jahr' },
     categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
-    events: []
+    events: [],
+    eventsTotal: null,
+    event: {}
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
@@ -20,11 +31,36 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit('SET_EVENTS', response.data)
+          commit('SET_EVENTS_TOTAL', response.headers['x-total-count'])
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      var event = getters.getEventById(id)
+
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data);
+          })
+          .catch(error => {
+            console.log("There was an error:", error.response);
+          });
+      }
     }
   },
   getters: {
-    catLength: state => {
-      return state.categories.length
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
     }
   }
 })
